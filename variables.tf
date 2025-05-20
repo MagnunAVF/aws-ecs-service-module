07406fb9bbb5c2a3cc74c2eccd7a661873b2bda9
MagnunAVF <magnunavf@gmail.com>
2025-05-20 04:10:44 -0300

    Initial terraform setup

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  type        = string
+  description = "Region where AWS resources will be provisioned."
+}
+
+variable "service_name" {
+  type        = string
+  description = "Name of the service to be used in ECS or similar identifier."
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the ECS cluster where the service will be deployed."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC where service-related resources will be provisioned."
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet IDs where the service will be deployed."
+}
+
+variable "service_port" {
+  type        = number
+  description = "Port on which the service will be accessible."
+}
+
+variable "service_cpu" {
+  type        = number
+  description = "Amount of CPU allocated to the service, specified in ECS CPU units."
+}
+
+variable "service_memory" {
+  type        = number
+  description = "Amount of memory allocated to the service, specified in MB."
+}
+
+variable "service_listener" {
+  type        = string
+  description = "ARN of the Application Load Balancer listener that will be used by the service."
+}
